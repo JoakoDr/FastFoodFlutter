9780a17f920d523ce11bd84dafca7ff521f481f9
Joaquin <joaco_diaz_ramirez@hotmail.com>
2019-01-23 14:27:39 +0100

    Card done

--- lib/FastFoodDetails.dart
+++ lib/FastFoodDetails.dart
@@ -17,45 +17,29 @@ class _FastDetailsState extends State<FastDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Fast Food Restaurants')),
-      body: _buildBody(context),
-    );
-  }
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Card(
+              child:
+                  new Column(
+            children: <Widget>[
+              new Icon(Icons.home, size: 100.0,color: Colors.red,),
+              new Text("Compañia: "+widget.restaurant.name),
+              new Text("Tipo de Comida : "+widget.restaurant.food),
+              new Text("Calorias: "+widget.restaurant.calories)
+            ],
+        )
 
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('restaurants').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-        return _buildList(context, widget.restaurant);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, Restaurant restaurant) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: data.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    // ignore: argument_type_not_assignable
-    final restaurant = widget.restaurant;
-
-    return Padding(
-      key: ValueKey(restaurant.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(restaurant.name),
-          trailing: Text(restaurant.calories),
-          onTap: () => print(restaurant)
+            )
+          ],
         ),
       ),
     );
   }
+
+
+
+
 }
